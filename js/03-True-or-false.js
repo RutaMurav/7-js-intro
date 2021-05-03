c1c1ff78bb7735c0ee7220108514c0497c6b7ecd
RutaMurav <82802755+RutaMurav@users.noreply.github.com>
2021-05-03 21:13:39 +0300

    true fallse try

---
+++ js/03-True-or-false.js
@@ -0,0 +1,15 @@
+const Pomidoras = true;
+console.log(Pomidoras);
+
+const BandykDarKarta = false;
+console.log(BandykDarKarta);
+
+const kurisDidesnis = [9, 6, 4, 2, 5];
+console.log(kurisDidesnis);
+const 
+
+
+const kurisMazesnis = [9, 6, 4, 2, 5] 
+
+
+
