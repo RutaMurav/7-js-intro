76556689feb41056474c0967f4dbbc00f525b707
RutaMurav <82802755+RutaMurav@users.noreply.github.com>
2021-05-04 13:49:01 +0300

    lenght

---
+++ js/06-length.js
@@ -0,0 +1,14 @@
+const hi = 'Labas';
+const hiSize = hi.length;
+
+//Teksto "labas" ilgis yra 5 simboliai
+
+const ans = `Tekto "${hi}" ilgis yra ${hiSize} simboliai.`;
+console.log(ans);
+
+const marks = [10, 2, 8, 4];
+const marksCount = marks.length;
+
+const student = `Studentas turi ${marksCount} pazymius.`;
+console.log(student);
+
