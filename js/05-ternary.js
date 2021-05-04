84eef3c559614927f5d96c7669ca6d8cd5d73ee6
RutaMurav <82802755+RutaMurav@users.noreply.github.com>
2021-05-04 12:27:28 +0300

    ternary

---
+++ js/05-ternary.js
@@ -0,0 +1,22 @@
+/*
+Ternary - sutrumpintas if'as
+jam yra butinos true ir false reiksmes
+struktura:
+klausimas? jei tiesa: jei netiesa
+*/
+
+const vartotojoAmzius = 14;
+const amziusCenzas = 18;
+
+const arIleistiISpecialiaVieta = vartotojoAmzius >= amziusCenzas ? true : false;
+console.log('ar ileisti:', arIleistiISpecialiaVieta);
+
+
+const amzius = 14;
+const cenzas = 18;
+
+const arVyresnis = amzius >= cenzas ? 'vyresnis' : 'ne vyresnis';
+console.log('ar vyresnis:', arVyresnis);
+
+
+
