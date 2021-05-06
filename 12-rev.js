fda133886b7b5f415d7763ca2440b5df5827b3f5
RutaMurav <82802755+RutaMurav@users.noreply.github.com>
2021-05-06 10:36:17 +0300

    reverser alt

--- 12-rev.js
+++ 12-rev.js
@@ -12,3 +12,11 @@ for(let i = tekstas.length - 1; i >= 0; i--) {
 }
 
 console.log(tekstas, '->', reverse);
+
+// for(let i = tekstas.length - 1; i >= 0; i--) {
+ //   reverse += tekstas[i];
+//}
+//for(let i = 0; i < tekstas.length; i++){
+//    reverse = tekstas[i] + reverse;
+//}
+//for (let i = 0; i < tekstas.lenght - 1; i>=0;i--
