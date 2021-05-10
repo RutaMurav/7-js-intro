0e53448e536e79be1e5759c175d0fdbfb2c031d9
RutaMurav <82802755+RutaMurav@users.noreply.github.com>
2021-05-10 11:42:21 +0300

    bandomieji

---
+++ 17-bandomieji.js
@@ -0,0 +1,18 @@
+function check(a, x) {
+
+    for(let i=0; i<a.length; i++ ){
+  
+      const lego = a[i]; 
+  
+      if(lego == x) {
+  
+             return true
+  
+      }
+  
+    } 
+  
+    return false;
+  
+  }
+
